8ee23cbd2dbdcca908b9a9da8a77357af21d32b9
jay.jia <jay.jia@shopji.com>
2022-03-23 12:39:30 +0800

    footer root link to open without blank

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -8,7 +8,7 @@
 
                     <a target="_blank" :href="'https://shopji.co/partners/'+$i18n.locale"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">{{$t('footer.partner')}}</font></font></a>
 
-                    <nuxt-link target="_blank" :to="localePath('/')"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">{{$t('footer.policy')}}</font></font></nuxt-link>
+                    <nuxt-link :to="localePath('/')"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">{{$t('footer.policy')}}</font></font></nuxt-link>
 
 
 
